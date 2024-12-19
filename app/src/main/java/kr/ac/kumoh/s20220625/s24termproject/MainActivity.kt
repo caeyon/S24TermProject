7bad3047484c1086e0bfc1fb6da209325c88e5b6
caeyon <shallyshin@naver.com>
2024-12-19 22:10:56 +0900

    🚧 AuthorList 완성

--- app/src/main/java/kr/ac/kumoh/s20220625/s24termproject/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20220625/s24termproject/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kr.ac.kumoh.s20220625.s24termproject.ui.theme.S24TermProjectTheme
 
@@ -33,7 +42,7 @@ fun MainScreen(viewModel: WebtoonViewModel = viewModel()) {
     val authorList by viewModel.authorList.observeAsState(emptyList())
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        AuthorResult(
+        AuthorList(
             list = authorList,
             modifier = Modifier.padding(innerPadding)
         )
@@ -41,10 +50,37 @@ fun MainScreen(viewModel: WebtoonViewModel = viewModel()) {
 }
 
 @Composable
-fun AuthorResult(list: List<Author>, modifier: Modifier) {
-    Column(
-        modifier
+fun AuthorList(list: List<Author>, modifier: Modifier) {
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp), //각 아이템 사이에 일정한 간격을 두고 배치
+        contentPadding = PaddingValues(horizontal = 8.dp) //콘텐츠(각 아이템) 내부의 패딩
     ) {
-        Text(list.toString())
+        items(list) { author ->
+            AuthorItem(author)
+        }
     }
+}
+
+@Composable
+fun AuthorItem(author: Author) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xffffffcc))
+            .padding(16.dp)
+    ) {
+        TextName(author.name)
+        TextBirth(author.birth)
+    }
+}
+
+@Composable
+fun TextName(name: String) {
+    Text(name, fontSize = 30.sp)
+}
+
+@Composable
+fun TextBirth(birth: String) {
+    Text(birth, fontSize = 20.sp)
 }
